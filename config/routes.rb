9fbc4e6dcfcbe3de857111fda51f07c61a290f4f
willr727 <whmrogers@googlemail.com>
2018-01-16 18:00:23 +0000

    Add comments to posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
+  resources :posts do
+  	resources :comments
+  end
+
 
   root 'posts#index' #set site to default to posts
 end
