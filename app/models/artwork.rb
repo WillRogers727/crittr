86675ee0e3644949a35d1602186552ba10804bb3
willr727 <whmrogers@googlemail.com>
2018-01-18 17:11:43 +0000

    basic gallery styling and thumbnails

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -6,6 +6,6 @@ class Artwork < ApplicationRecord
 	# has_attached_file :image, styles: { medium: "700x500#", thumb: "350x250>" }
 	# validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
-	has_attached_file :image, styles: { medium: "400x250#", thumb: "100x100>" }
+	has_attached_file :image, styles: { medium: "400x250#", thumb: "250x150#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
