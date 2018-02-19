cf761b92b5876864d1c2b8e8bd84f4c0a4b86c6a
willr727 <whmrogers@googlemail.com>
2018-02-19 11:48:45 +0000

    undo conversations migration

--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,14 @@ ActiveRecord::Schema.define(version: 20180205133738) do
     t.index ["cached_weighted_total"], name: "index_artworks_on_cached_weighted_total"
   end
 
+  create_table "comment_hierarchies", force: :cascade do |t|
+    t.integer "ancestor_id", null: false
+    t.integer "descendant_id", null: false
+    t.integer "generations", null: false
+    t.index ["ancestor_id", "descendant_id", "generations"], name: "comment_anc_desc_udx", unique: true
+    t.index ["descendant_id"], name: "comment_desc_idx"
+  end
+
   create_table "comments", force: :cascade do |t|
     t.text "comment"
     t.integer "user_id"
@@ -42,6 +50,7 @@ ActiveRecord::Schema.define(version: 20180205133738) do
     t.datetime "updated_at", null: false
     t.integer "commentable_id"
     t.string "commentable_type"
+    t.integer "parent_id"
     t.integer "cached_votes_total", default: 0
     t.integer "cached_votes_score", default: 0
     t.integer "cached_votes_up", default: 0
