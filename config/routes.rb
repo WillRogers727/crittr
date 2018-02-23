ae1b925d27f600377ac1746f082b10ef5600cc7a
willr727 <whmrogers@googlemail.com>
2018-02-23 14:38:44 +0000

    routes fixed, comments now working :D

--- config/routes.rb
+++ config/routes.rb
@@ -3,33 +3,37 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#home' #set site to default to the new home page
 
-
   resources :categories, shallow: true do 
-    
-    resources :posts do
-      resources :comments, module: :posts do
-      end
-      member do
-        put "like", to: "posts#upvote"
-        put "dislike", to: "posts#downvote"
-        put 'set_answered' #put is used to update values
-      end
-    end   
-
+    resources :posts
     resources :artworks do
-      resources :comments, module: :artworks do
-      end
       resources :pictures
       member do
-        put "like", to: "artworks#upvote"
-        put "dislike", to: "artworks#downvote"
         patch 'create_picture'
       end
-    end 
+    end
+  end #end of categories
+  
+  resources :posts do
+    resources :comments, module: :posts do
+    end
+    member do
+      put "like", to: "posts#upvote"
+      put "dislike", to: "posts#downvote"
+      put 'set_answered' #put is used to update values
+    end
   end
 
+  resources :artworks do
+    resources :comments, module: :artworks do
+    end
+    member do
+      put "like", to: "artworks#upvote"
+      put "dislike", to: "artworks#downvote"
+      patch 'create_picture'
+    end
+  end 
   
-   resources :comments do
+   resources :comments do #is this needed??
       member do
         put "like", to: "comments#upvote"
         put "dislike", to: "comments#downvote"
