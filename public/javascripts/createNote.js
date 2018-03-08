127092d4978c74583b1af44613e36832b56563d4
willr727 <whmrogers@googlemail.com>
2018-03-08 20:25:12 +0000

    js tweaks

--- public/javascripts/createNote.js
+++ public/javascripts/createNote.js
@@ -1,26 +1,30 @@
 $(document).ready(function() {
 	// alert("js fucntion check");
 	var container = document.querySelector('.noteArtImageGallery');
-
 	var canvas = document.querySelector('canvas')
 	var ctx = canvas.getContext('2d');
+
+	// initialize width and height
 	setCanvasWidth(canvas);
 	setCanvasHeight(canvas, container);
-	$(window).resize(function () {
+	
+	// as the window changes with, adjust height of the canvas and container
+	$(window).resize(function () { 
     setCanvasHeight(canvas,container);
-});
+	});
 	
 
 });
 
-function setCanvasWidth(canvas){
+// sets the width of the canvas to 100%
+function setCanvasWidth(canvas){ 
 	canvas.style.width='100%';
 	// canvas.style.height='100px';
 	canvas.width  = canvas.offsetWidth;
 	// canvas.height = canvas.offsetHeight;
 }
 
-
+// sets canvas height and container height to the same as the image
 function setCanvasHeight(canvas, container){
 	var h = $('#canvasImg').height();
 
