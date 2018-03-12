81d7446e423e8a157a53f9b0f1bce3cd030757d7
willr727 <whmrogers@googlemail.com>
2018-03-12 16:41:25 +0000

    wrestling with css and javascript

--- public/javascripts/artShow.js
+++ public/javascripts/artShow.js
@@ -1,21 +1,41 @@
 $(document).ready(function() {
 	
-	var container = document.querySelector('.artImageGallery');
+	$('.image-1').addClass('visible');
+	$('.image-1').removeClass('hidden');
+	var holder = document.querySelector('.image-1 > .artImageHolder');
+	var image = document.querySelector('.image-1 > .artImageHolder img');
+	setContHeight(initialHolder, initialImage); //initialise height with image 1 - not running?
 
-	setContHeight(container);
+	
 
-	$(window).resize(function () { 
-	   setContHeight(container);
+	$('.artToggle').change(function() {
+			var num = this.value;
+			var imgClass = "image-"+num;
+			$('.artImage').removeClass('visible');
+			$('.artImage').addClass('hidden');
+			$('.'+imgClass).addClass('visible');
+			$('.'+imgClass).removeClass('hidden');
+			image = $('.'+imgClass+' > .artImageHolder img');
+			holder = $('.'+imgClass+' > .artImageHolder');
+			setContHeight(image, holder);
 	});
 
 
+	$(window).resize(function () { 
+	   setContHeight(image, holder);
+	});
+		//the current image holder
+		
 
+		// the current image
 
-	// sets container height to the same as the image
-	function setContHeight(container){
-		var h = $('.artImage').height();
-
-		container.style.height= h + 'px';
-		container.height = container.offsetHeight;
+	// sets image holder height to the same as the image
+	
+	function setContHeight(image, holder){
+		alert("fuck my tits");
+		var h = image.height();
+		
+		holder.style.height = h + 'px';
+		holder.height = holder.offsetHeight;
 	}
 });
