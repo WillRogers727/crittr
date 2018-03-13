730bbe8d6a6ea3f899e405b1b7347499a087fc94
willr727 <whmrogers@googlemail.com>
2018-03-13 17:54:54 +0000

    Added show/hide notes

--- public/javascripts/artShow.js
+++ public/javascripts/artShow.js
@@ -3,12 +3,10 @@ $(document).ready(function() {
 	var activeHolder;
 	var activeImage;
 
-	
-
 	$('.image-1').addClass('visible');
 	$('.image-1').removeClass('hidden');
 	
-
+	//images are toggled
 	$('.artToggle').change(function() {
 			var num = this.value;
 			var imgClass = "image-"+num;
@@ -21,47 +19,56 @@ $(document).ready(function() {
 
 			getActiveImage();
 			setHolderHeight();
-
-			// $(activeImage).addClass('yo');
-			// $(activeHolder).addClass('yoHolder');
 	});
 
-	getActiveImage();
-	setHolderHeight();
+
 
 	//need to make the art holder height the same as the image inside it
 	//when - 
 	//page is first loaded
-
+	getActiveImage();
+	setHolderHeight();
 	//window is resized
 	$(window).resize(function () { 
 	  getActiveImage();
 		setHolderHeight();
 	});
-	//images are toggled
-
-
 
 	//function to get current active image and therefore holder
 	function getActiveImage() {
-		// alert("get active image started");
 		activeImage = document.querySelector('.visible .artImageHolder img');
 		activeHolder = document.querySelector('.visible .artImageHolder');
 	}
 	//function to set the size
 	function setHolderHeight() {
-		// alert("holder setting");
 		var h = $(activeImage).height();
-		// alert(h);
 		activeHolder.style.height = h + 'px';
 		activeHolder.height = activeHolder.offsetHeight;
 	}
 
 
-	// var holder = document.querySelector('.image-1 > .artImageHolder');
-	// var image = document.querySelector('.image-1 > .artImageHolder img');
-	// setContHeight(initialHolder, initialImage); //initialise height with image 1 - not running?
+	$('.noteShow').click(function() {
+		var val = this.name;
+		showNote(val);
+	});
 
+	$('.noteHide').click(function() {
+		var val = this.name;
+		hideNote(val);
+	});
+
+	function showNote(val) {
+		$('.noteImg-' + val).removeClass('hiddenNote');
+		$('#nShow-' + val).addClass('hiddenBtn');
+		$('#nHide-' + val).removeClass('hiddenBtn');
+	}
+
+	function hideNote(val) {
+		$('.noteImg-' + val).addClass('hiddenNote');
+		$('#nHide-' + val).addClass('hiddenBtn');
+		$('#nShow-' + val).removeClass('hiddenBtn');
+
+	}
 
 
 });
