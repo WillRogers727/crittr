853af3b13b303eee86d473f7601c3a03db8f305f
willr727 <whmrogers@googlemail.com>
2018-03-17 18:11:32 +0000

    updated migration

--- db/migrate/20180317180512_add_category.rb
+++ db/migrate/20180317180512_add_category.rb
@@ -1,9 +1,9 @@
 class AddCategory < ActiveRecord::Migration[5.1]
   def self.up
-    Category.create{:name => "All"}
-    Category.create{:name => "Fine Art"}
-    Category.create{:name => "Digital Art"}
-    Category.create{:name => "3D"}
+    Category.create{:name => 'All'}
+    Category.create{:name => 'Fine Art'}
+    Category.create{:name => 'Digital Art'}
+    Category.create{:name => '3D'}
    end
 
   def self.down
