c7f10e89c38eb20ab1c2b97871bb40252009ade6
willr727 <whmrogers@googlemail.com>
2018-03-17 18:20:10 +0000

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create!(:name => "All")
+Category.create!(:name => "Fine Art")
+Category.create!(:name => "3d")
