3f9f4acaef0e847d4e46d41184658de0beaf1129
willr727 <whmrogers@googlemail.com>
2018-03-19 11:12:21 +0000

    migration to add cats

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,10 +39,6 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 
 namespace :deploy do
 
-	task :seed do
-		run "cd #{current_path}; rake db:seed RAILS_ENV=#{rails_env}"
-	end
-
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
