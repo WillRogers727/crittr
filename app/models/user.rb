99f11185821ca5882456b44ccb7f2d7d1442e1f1
willr727 <whmrogers@googlemail.com>
2018-03-19 19:58:45 +0000

    aaaaaaaaaaaaaaaaaaaa

--- app/models/user.rb
+++ app/models/user.rb
@@ -57,13 +57,22 @@ class User < ApplicationRecord
     following.include?(other_user)
   end
 
+  # def self.from_omniauth(auth)
+  # where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  #   user.email = auth.info.email
+  #   user.provider = auth.provider
+  #   user.uid = auth.uid
+  #   user.password = Devise.friendly_token[0,20]
+  # end
+  # end
+
   def self.from_omniauth(auth)
-  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-    user.email = auth.info.email
-    user.provider = auth.provider
-    user.uid = auth.uid
-    user.password = Devise.friendly_token[0,20]
-  end
+    where(email: auth.info.email).first_or_initialize.tap do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.username = auth.info.name
+      user.save
+    end
 end
 
 end
