6107ad7cf51401ea6115c6c76157913ba30a6dee
willr727 <whmrogers@googlemail.com>
2018-03-19 19:20:20 +0000

    route fix for fb login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :views
-  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'pages#home' #set site to default to the new home page
 
   resources :categories, shallow: true do 
