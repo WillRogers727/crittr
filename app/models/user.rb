276376fee35549761c1c5d10b15dda66210f05d7
willr727 <whmrogers@googlemail.com>
2018-03-20 14:34:28 +0000

    tweak bug fix with password

--- app/models/user.rb
+++ app/models/user.rb
@@ -57,15 +57,6 @@ class User < ApplicationRecord
     following.include?(other_user)
   end
 
-  # def self.from_omniauth(auth)
-  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-  #     user.email = auth.info.email
-  #     user.provider = auth.provider
-  #     user.uid = auth.uid
-  #     user.password = Devise.friendly_token[0,20]
-  #   end
-  # end
-
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
@@ -81,7 +72,6 @@ class User < ApplicationRecord
     update_attributes(
         provider: auth.provider,
         uid: auth.uid
-        password: "password"
       )
   end
 
