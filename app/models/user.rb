37b87a1fe4d01daa4c87fee7d974b41f0f476d74
willr727 <whmrogers@googlemail.com>
2018-03-20 10:49:53 +0000

    fb login fixes for testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -58,17 +58,19 @@ class User < ApplicationRecord
   end
 
   # def self.from_omniauth(auth)
-  # where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-  #   user.email = auth.info.email
-  #   user.provider = auth.provider
-  #   user.uid = auth.uid
-  #   user.password = Devise.friendly_token[0,20]
-  # end
+  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  #     user.email = auth.info.email
+  #     user.provider = auth.provider
+  #     user.uid = auth.uid
+  #     user.password = Devise.friendly_token[0,20]
+  #   end
   # end
 
   def self.from_omniauth(auth)
-    where(email: auth.info.email).first_or_initialize.tap do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
       user.email = auth.info.email
+      user.provider = auth.provider
+      user.uid = auth.uid
       user.password = Devise.friendly_token[0,20]
       user.username = auth.info.name
       user.save
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :views
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  authenticated :user do
+    root 'pages#home', as: 'authenticated_root'
+  end
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
+
   root 'pages#home' #set site to default to the new home page
 
   resources :categories, shallow: true do 
