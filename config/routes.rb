d009bfed7617a1344a0b38ea989b062243530e00
willr727 <whmrogers@googlemail.com>
2018-03-20 11:53:36 +0000

    update without password if facebook user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :views
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
   authenticated :user do
     root 'pages#home', as: 'authenticated_root'
   end
