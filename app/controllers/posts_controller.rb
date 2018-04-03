954ffcd799f69dceb725cac31f2603f88a68875b
Will Rogers <whmrogers@googlemail.com>
2018-04-03 18:34:39 +0100

    adding nsfw tags

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,19 +12,19 @@ class PostsController < ApplicationController
 			if params[:tag]
 				@allPosts = Post.all
 				@search = @allPosts.tagged_with(params[:tag]).ransack(params[:q])
-		  	@posts = @search.result.paginate(page: params[:page], per_page: 10)
+		  		@posts = @search.result.paginate(page: params[:page], per_page: 25)
 			else
 				@allPosts = Post.all
 				@search = @allPosts.ransack(params[:q])
-		  	@posts = @search.result.paginate(page: params[:page], per_page: 10)
+		  		@posts = @search.result.paginate(page: params[:page], per_page: 25)
 			end
 		else
 			if params[:tag]
 				@search = @category.posts.tagged_with(params[:tag]).ransack(params[:q])
-	  		@posts = @search.result.paginate(page: params[:page], per_page: 10)
+	  			@posts = @search.result.paginate(page: params[:page], per_page: 25)
 			else
 				@search = @category.posts.ransack(params[:q])
-	  		@posts = @search.result.paginate(page: params[:page], per_page: 10)
+	  			@posts = @search.result.paginate(page: params[:page], per_page: 25)
 			end
 		end
 
