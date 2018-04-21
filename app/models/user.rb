2c139ce89c39fbf8238bc2ab7dad7045074894b7
willr727 <whmrogers@googlemail.com>
2018-04-21 14:19:11 +0100

    blankavatar fix attempt

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,9 @@ class User < ApplicationRecord
 
   acts_as_voter
 
-  has_attached_file :avatar, styles: { medium: "200x200#", thumb: "50x50#" }, default_url: ActionController::Base.helpers.asset_path("blankAvatar.png")
+  has_attached_file :avatar, styles: { medium: "200x200#", thumb: "50x50#" }, default_url: => "blankAvatar.png"
+
+  #ActionController::Base.helpers.asset_path
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   has_many :conversations, :foreign_key => :sender_id, dependent: :destroy
