4711cd11496b9fb7417d63ea300214fb5ec33540
Will Rogers <whmrogers@googlemail.com>
2018-04-21 21:56:03 +0100

    added full categories

--- db/migrate/20180421122638_add_cats.rb
+++ db/migrate/20180421122638_add_cats.rb
@@ -1,9 +1,16 @@
 class AddCats < ActiveRecord::Migration[5.1]
   def up
     Category.create(id: 1, name: "All");
-    Category.create(id: 2, name: "Fine Art");
-    Category.create(id: 3, name: "3D");
-    Category.create(id: 4, name: "Digital Painting");
+    Category.create(id: 2, name: "Concept Art");
+    Category.create(id: 3, name: "Digital Art");
+    Category.create(id: 4, name: "3D");
+    Category.create(id: 5, name: "Photography");
+    Category.create(id: 6, name: "Traditional Art");
+    Category.create(id: 7, name: "UI Design");
+    Category.create(id: 8, name: "Architecture");
+    Category.create(id: 9, name: "Web Design");
+    Category.create(id: 10, name: "Fan Art");
+    Category.create(id: 11, name: "Typography");
   end
 
   def down
