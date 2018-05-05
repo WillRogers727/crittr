0d5e09738668d0e21646ad9fd8a60b05ccbae037
willr727 <whmrogers@googlemail.com>
2018-05-05 17:51:53 +0100

    removed turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require turbolinks
 //= require jquery3
-//= require jquery.turbolinks
 //= require jquery_ujs
 //= require masonry/jquery.masonry
 //= require sweetalert2
