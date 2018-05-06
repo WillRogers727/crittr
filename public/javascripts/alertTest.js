878775f04bfa338212e7a2206b5db2e8d696ce25
willr727 <whmrogers@googlemail.com>
2018-05-06 19:52:44 +0100

    solution testing 1

--- public/javascripts/alertTest.js
+++ public/javascripts/alertTest.js
@@ -1,3 +1,27 @@
 $(document).on('ready turbolinks:load', function() {
 	alert("working");
+
+	$("a.contentToggle.NoteToggle").on('click', function () {
+		// alert("toggle clicked");
+	  $('div.noteContent').slideToggle(200).toggleClass('contentActive');
+	  $("a.contentToggle.NoteToggle > .toggleArrow").toggleClass('toggleArrowActive');
+	});
+
+	$(".commentToggle").on('click', function () {
+		// alert("toggle clicked");
+	  $('div.commentShrinkSection').slideToggle(200).toggleClass('contentActive');
+	  $(".commentToggle > .darkToggleArrow").toggleClass('toggleArrowActive');
+	});
+
+	$(".reviewToggle").on('click', function () {
+		// alert("toggle clicked");
+	  $('div.reviewShrinkSection').slideToggle(200).toggleClass('contentActive');
+	  $(".reviewToggle > .darkToggleArrow").toggleClass('toggleArrowActive');
+	});
+
+	$(".addToggle").on('click', function () {
+		// alert("toggle clicked");
+	  $('div.addContent').slideToggle(200).toggleClass('contentActive');
+	  $(".addToggle > .darkToggleArrow").toggleClass('toggleArrowActive');
+	});
 });
